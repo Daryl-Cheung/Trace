a05ff37a1846902faceaaf2dc658d1d98723c267
Daryl <cheungdaryl@gmail.com>
2026-09-09 17:05:55 -0700

    feat: implement sign in logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -48,7 +48,34 @@ export const signUp = async (req, res, next) => {
 }
 
 export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        return next(error);
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+    if (!isPasswordValid) {
+        const error = new Error('Invalid password');
+        error.statusCode = 401;
+        return next(error);
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+    res.status(200).json({
+        success: true,
+        message: 'User signed in successfully',
+        data: {
+            token,
+            user,
+        }
+    });
 }
 
 export const signOut = async (req, res, next) => {
