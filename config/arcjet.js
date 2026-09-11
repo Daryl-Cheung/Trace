1f414d5212f0af394245e94677ac9dd6fd61ddf9
Daryl <cheungdaryl@gmail.com>
2026-09-10 23:44:41 -0700

    feat: implement rate limiting and bot detection middleware

---
+++ config/arcjet.js
@@ -0,0 +1,31 @@
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import { ARCJET_KEY, ARCJET_ENV } from "./env.js";
+
+
+const aj = arcjet({
+  // Get your site key from https://console.arcjet.com and set it as an environment
+  // variable rather than hard coding.
+  key: ARCJET_KEY,
+  env: ARCJET_ENV,
+  characteristics: ["ip.src"],
+  rules: [
+
+    shield({ mode: "LIVE" }),
+
+    detectBot({
+      mode: "LIVE", 
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
+    }),
+  ],
+});
+
+export default aj;
